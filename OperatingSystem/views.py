ac616182003b6c71414f358f88f3333b2045740b
Yahya <yahyabinzakariyya@gmail.com>
2023-05-10 15:56:43 +0500

    static files added

--- OperatingSystem/views.py
+++ OperatingSystem/views.py
@@ -1,8 +1,18 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    data = {
+        "title":"Operating Systems",
+    }
+    return render(request, 'index.html', data)
 
-# def process(request):
-#     return HttpResponse("This is process button")
+def process(request):
+    data = {
+        "title":"Process Management",
+        "processes":[
+            {"pid":1,"at":0,"bt":4},
+            {"pid":2,"at":2,"bt":3},
+            {"pid":3,"at":5,"bt":7}
+        ]
+    }
+    return render(request, "process.html",data)
